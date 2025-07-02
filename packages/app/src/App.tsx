1d10a48cbdef9f48aee67eba8777fcf0797bbe59
saurabhdaware <saurabhdaware99@gmail.com>
2025-07-02 11:36:07 +0530

    feat: add whitelabelling

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -3,7 +3,17 @@ import { Button } from "nexus-library";
 function App() {
   return (
     <div className="light">
-      <Button variant="tertiary">Hello</Button>
+      <Button variant="primary">Hello</Button>
+      <input
+        type="color"
+        onChange={(e) => {
+          console.log(e.currentTarget.value);
+          document.documentElement.style.setProperty(
+            "--colors-azure-700",
+            e.currentTarget.value
+          );
+        }}
+      />
     </div>
   );
 }
